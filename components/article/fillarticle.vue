bcc95fcbf251c2b0cc9461455cebffe8476f92fc
Syahrul <syahrulrizal9999@gmail.com>
2018-10-16 05:02:23 +0700

    Update isi article

--- components/article/fillarticle.vue
+++ components/article/fillarticle.vue
@@ -3,36 +3,112 @@
         <div class="uk-grid ">
             <div class="uk-width-1-1 uk-padding-remove-left">
                 <article class="uk-article uk-padding-large uk-padding-remove-bottom" style="margin-left:30px;">
-                    <h1 class="uk-article-header">Tak
-                        Perlu Khawatir Bila Ternyata Pilot Tidur selama Penerbangan</h1>
-                    <p class="uk-article-meta"><i>Agustus lalu, penumpang United Airlines menangkap foto pilot
-                            tidur di luar kokpit selama 90 menit. Foto tersebar dan seketika dunia maya heboh
-                            karenanya.</i></p>
-                    <p><a class="" href="#">Himas Nur</a></p>
-                    <img class="uk-width-1-1" data-src="https://phinemo.com/wp-content/uploads/2018/09/2-10-e1537244208124.jpeg" alt="" uk-img>
-                    <p class="uk-article-meta uk-margin-remove-top uk-margin-bottom"><cite>Ilustrasi Awak
-                            Penerbang Tertidur (Foto/Basith Subastian)</cite></p>
-                    <p class="uk-text-justify"> Mengutip <em>Travel and Leisure</em> pada Senin (17/9), pada bulan
-                        Agustus kemarin
-                        terjadi “drama di udara” ketika seorang pilot United Airlines tertangkap kamera
-                        sedang tertidur dalam pesawat dengan tujuan dari Amerika ke Glasglow, Skotlandia.
+                    <h1 class="uk-article-title">
+                        4 Desa Wisata Bali yang Asyik Anda Datangi Akhir Pekan Ini
+                    </h1>
+                    <h4 class="">Paduan suasana alam hingga kultur masyarakat setempat merupakan paket 
+                        lengkap yang dapat Anda nikmati saat bertandang ke desa wisata Bali</h4>
+                        <p class="uk-article-meta"> Ditulis Oleh :<a class="" href="#" style="color : red;"> Himas Nur</a></p>
+                            <div class="uk-flex uk-flex-center uk-width-1-1@s uk-width-1-1@m">
+                                <img class="uk-width-1-2@s uk-width-3-4@m uk-height-1-1" data-src="https://phinemo.com/wp-content/uploads/2018/09/1-100-e1537424320156.jpg" alt="" uk-img>
+                            </div>
+                            <p class="uk-article-meta uk-margin-bottom uk-text-center">
+                                <cite>desa wisata penglipuran di bali (Foto/baliberkarya)</cite>
+                            </p>
+
+                    <p class="uk-text-justify"> 
+                        Desa Wisata Bali selalu memiliki pesona tersendiri bagi para wisatawan yang berkunjung ke Pulau Dewata, Bali. 
+                        Paduan suasana alam hingga kultur masyarakat setempat merupakan paket lengkap yang patut 
+                        Anda nikmati di akhir pekan ini.
                     </p>
-                    <p class="uk-text-justify">Foto yang telah tersebar itu sontak menjadi tanda tanya dan membuat
-                        heboh,&nbsp;lantaran berpikir jika pilot tidur siapa yang mengendalikan pesawat.</p>
-                    <img class="uk-width-1-1" data-src="https://phinemo.com/wp-content/uploads/2018/09/1-68-e1537244315445.jpg" alt=""
-                        uk-img>
-                    <p class="uk-article-meta"><cite>(Istockphoto/Humonia)</cite></p>
-                    <p class="uk-text-justify">Seorang penumpang yang mengambil foto tersebut mengaku melihat sang
-                        pilot tertidur
-                        di luar kokpit selama 90 menit. Sebelum memejamkan mata, penumpang mengaku melihat
-                        sang pilot pergi ke toilet dan mengganti bajunya.</p>
-                    <p class="uk-text-justify">Walaupun terdengar mengerikan, sesungguhnya kondisi tidurnya pilot
-                        merupakan hal
-                        yang tak perlu dikhawatirkan.&nbsp;Sebenarnya, sama seperti penumpang, pilot pun
-                        punya hak untuk beristirahat.</p>
-                    <p class="uk-text-justify">The Federal Aviation Administration, sebuah badan administrasi AS,
-                        justru meminta
-                        para awak penerbang untuk istirahat saat melakukan penerbangan panjang.</p>
+                    <p class="uk-text-justify"> 
+                        Berikut adalah beberapa rekomendasi destinasi desa wisata Bali 
+                        yang menarik dan bisa Anda jadikan rujukan dalam bucket list akhir pekan Anda.
+                    </p>
+                    <h2 class="uk-text-bold">1. Desa Ubud</h2>
+                        <div class="uk-flex uk-flex-center uk-width-1-1@s uk-width-1-1@m">
+                             <img class="uk-width-1-2@s uk-width-3-4@m uk-height-1-1" 
+                             data-src="https://phinemo.com/wp-content/uploads/2018/09/1-99-e1537423881797.jpg" alt="" uk-img>
+                        </div>
+                        <p class="uk-article-meta uk-margin-bottom uk-text-center">
+                             <cite>desa wisata ubud di bali (Foto/baliberkarya)</cite>
+                        </p>
+                    
+                    <p class="uk-text-justify">Desa Wisata Bali yang satu ini sudah cukup populer di kalangan wisatawan lokal dan mancanegara, 
+                        yakni Desa Ubud. Desa ini menawarkan pemandangan alam yang sangat indah dengan hawa sejuk yang jarang ditemui.
+                    </p>
+                    <p class="uk-text-justify">
+                        Tak hanya soal pemandangan alam, Ubud juga terkenal dengan pusat keseniannya. Anda dapat menikmati beragam karya seni 
+                        seperti seni luis, patung, tari serta ukiran di sini.
+                    </p>
+
+                    <h2 class="uk-text-bold">2. Desa Panglipuran</h2>
+                        <div class="uk-flex uk-flex-center uk-width-1-1@s uk-width-1-1@m">
+                            <img class="uk-width-1-2@s uk-width-3-4@m uk-height-1-1" 
+                            data-src="https://phinemo.com/wp-content/uploads/2018/09/1-100-e1537424320156.jpg" alt="" uk-img>
+                        </div>
+                        <p class="uk-article-meta uk-margin-bottom uk-text-center">
+                            <cite>desa wisata penglipuran di bali (Foto/baliberkarya)</cite>
+                        </p>
+                     <p class="uk-text-justify"> 
+                        Desa Penglipuran tak kalah populer dengan Desa Ubud, karena selain lokasinya sering dijadikan destinasi wisata, 
+                        tempat ini juga sering dijadikan sebagai latar syuting FTV (film televisi).
+                    </p>
+                     <p class="uk-text-justify"> 
+                        Desa Penglipuran memiliki luas sebesar 112 hektare, namun hanya sekitar 9 hektarenya yang difungsikan 
+                        sebagai rumah warga.
+                    </p>
+                     <p class="uk-text-justify"> 
+                        Ada yang perlu diperhatikan ketika Anda berencana bertandang ke sini, yakni Anda tak diperbolehkan untuk
+                         membawa kendaraan bermotor di sini.
+                    </p>
+                     <p class="uk-text-justify"> 
+                        Oleh sebab itu Desa Penglipuran ini sangat bersih dan bebas dari polusi udara. Tak heran bila desa ini 
+                        pernah mendapat penghargaan sebagi desa terbersih ketiga di dunia.
+                    </p>
+
+                    <h2 class="uk-text-bold">3. Desa Batubulan</h2>
+                        <div class="uk-flex uk-flex-center uk-width-1-1@s uk-width-1-1@m">
+                            <img class="uk-width-1-2@s uk-width-3-4@m uk-height-1-1" 
+                            data-src="https://phinemo.com/wp-content/uploads/2018/09/1-3-e1537424449613.png" alt="" uk-img>
+                        </div>
+                        <p class="uk-article-meta uk-margin-bottom uk-text-center">
+                            <cite>desa wisata batu bulan di bali (Foto/obyekwisataterlengkap)</cite>
+                        </p>
+                    <p class="uk-text-justify"> 
+                        Desa Batubulan merupakan salah satu desa wisata Bali yang punya kekayaan seni tinggi. 
+                        Desa ini terletak di Kabupaten Gianyar, Bali.
+                    </p>
+                    <p class="uk-text-justify"> 
+                        Di desa wisata ini, Anda bisa menonton pertunjukkan kesenian khas Bali seperti tari barong, legong, dan kecak. 
+                        Selain itu, desa ini juga memiliki aneka kerajinan yang menarik untuk oleh-oleh. Tak heran, 
+                        desa Batubulan selalu ramai wisatawan setiap harinya, apalagi pada saat musim liburan.
+                    </p>
+
+                    <h2 class="uk-text-bold">4. Desa Trunyan</h2>
+                        <div class="uk-flex uk-flex-center uk-width-1-1@s uk-width-1-1@m">
+                            <img class="uk-width-1-2@s uk-width-3-4@m uk-height-1-1" 
+                            data-src="https://phinemo.com/wp-content/uploads/2018/09/1-101.jpg" alt="" uk-img>
+                        </div>
+                        <p class="uk-article-meta uk-margin-bottom uk-text-center">
+                            <cite>desa wisata trunyan di bali (Foto/Pinterest)</cite>
+                        </p>
+                    <p class="uk-text-justify"> 
+                        Desa Trunyan merupakan salah satu desa wisata di Bali yang sangat fenomenal. Hal ini dikarenakan desa ini memiliki 
+                        tradisi yang cukup unik dan bagi sebagian orang mengerikan.
+                    </p>
+                    <p class="uk-text-justify"> 
+                        Desa tertua di Bali ini menganjurkan bahwa warga yang meninggal di desa ini, hanya boleh diletakkan 
+                        di ‘kandang’ bambu di sekitar pohon Trunyan.
+                    </p>
+                    <p class="uk-text-justify"> 
+                        Meski jenazah akan membusuk dan biasanya akan menimbulkan bau yang tak sedap, hal tersebut tak berlaku
+                        di Desa Trunyan. Sebabnya adalah desa ini memiliki pohon Trunyan yang mampu menutupi bau tak sedap tadi.
+                    </p>
+                    <p>Dapatkan ulasan menarik lainnya tentang <a href="https://phinemo.com/tag/desa-wisata/" class="uk-text-bold" style="color : red;">DESA WISATA</a>, 
+                    tulisan lain <a href="https://phinemo.com/author/himas-nur/" class="uk-text-bold" style="color : red;">Himas Nur</a>
+                    </p>
+                    
                 </article>
             </div>
         </div>
